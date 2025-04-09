683588a9968e58f67d20f415af1cf4c4b5b5fbeb
coderhs <mialme@hsps.in>
2025-04-09 21:25:29 +0530

    Ensure the default route is json

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
     controllers: {
       sessions: "users/sessions",
       registrations: "users/registrations"
-    }
+    }, defaults: { format: :json }
 
+  get "/secret", to: "confidential#secret", defaults: { format: :json }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
