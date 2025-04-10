bec7f10e7602d86c30bde7bf04e459c8626d1687
coderhs <mialme@hsps.in>
2025-04-10 10:17:50 +0530

    Show the user email in the API response
    Signed-off-by: coderhs <mialme@hsps.in>

--- app/controllers/confidential_controller.rb
+++ app/controllers/confidential_controller.rb
@@ -1,5 +1,5 @@
 class ConfidentialController < ApplicationController
   def secret
-    render json: { message: "I am not a fan of Hotwire!" }
+    render json: { message: "#{current_user.email}: I am not a fan of Hotwire!" }
   end
 end
